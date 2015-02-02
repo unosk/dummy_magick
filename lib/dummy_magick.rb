e924b84a65f4de149a6859a787db41b90a1fc555
unosk <unosk.s@gmail.com>
2015-02-03 01:13:31 +0900

    Write first source code and test

--- lib/dummy_magick.rb
+++ lib/dummy_magick.rb
@@ -1,5 +1,21 @@
 require 'dummy_magick/version'
+require 'base64'
+require 'mini_magick'
 
 module DummyMagick
-  # Your code goes here...
+  IMAGE_1X1 = Base64.decode64('R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==').freeze
+
+  ##
+  # Generate a dummy image file
+  #
+  # @param  width  [Integer] The width of the image.
+  # @param  height [Integer] The height of the image.
+  # @param  format [String]  The file extension of the image format. Like 'jpg', 'png', 'gif' etc.
+  # @return [File]
+  def self.dummy_image_file(width, height, format = :jpg)
+    img = ::MiniMagick::Image.create('gif') { |f| f.write IMAGE_1X1 }
+    img.resize("#{width}x#{height}!")
+    img.format(format)
+    File.new(img.path)
+  end
 end
--- spec/dummy_magick_spec.rb
+++ spec/dummy_magick_spec.rb
@@ -5,7 +5,21 @@ describe DummyMagick do
     expect(DummyMagick::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.dummy_image_file' do
+    subject { DummyMagick.dummy_image_file(10, 20, :png) }
+    let(:image) { MiniMagick::Image.new(subject.path) }
+
+    it 'returns an instance of File' do
+      expect(subject).to be_an_instance_of(File)
+    end
+
+    it 'generates an image with given size' do
+      expect(image.dimensions).to eq [10, 20]
+    end
+
+    it 'generates an image with given format' do
+      expect(File.extname(subject.path)).to eq '.png'
+      expect(image.mime_type).to eq 'image/png'
+    end
   end
 end
